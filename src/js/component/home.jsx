8ffd47cf81fa74f5a30b35ec3a7c192cc587a9eb
MiguelSEO1 <113799625+MiguelSEO1@users.noreply.github.com>
2023-01-05 22:00:46 +0100

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,7 +4,40 @@ import Light from "./lights";
 //create your first component
 const Home = () => {
 	const [lightUp, setLightUp] = useState (0);
+	const [running, setRunning] = useState(false);
 	const lights = ["danger", "warning", "success"];
+
+	const startLights = (index) => {
+		if (running === false ) {
+		setLightUp(index);
+		setTimeout(() => {
+		const nextIndex = (index + 1) % lights.length;
+		startLights(nextIndex);
+		}, 1000);
+		}
+		}
+		
+		useEffect(() => {
+			let timeoutId = null;
+			
+			const startLights = (index) => {
+			setLightUp(index);
+			if (running) {
+			const nextIndex = (index + 1) % lights.length;
+			timeoutId = setTimeout(() => startLights(nextIndex), 100);
+			}
+			}
+			
+			if (running) {
+			startLights(0);
+			} else {
+			clearTimeout(timeoutId);
+			}
+			
+			return () => clearTimeout(timeoutId);
+			}, [running]);
+
+			
 
 	return (
 		<div className="text-center">
@@ -16,6 +49,8 @@ const Home = () => {
 		      <button className="btn btn-primary m-3" onClick={() => {
 				lightUp == 2 ? setLightUp(0) : setLightUp(lightUp +1)
 			  }}>Traffic Lights</button>
+			  <button className={`btn ${running ? "bg-danger" : "bg-success"} m-3` } onClick={() => setRunning(!running)}>{running ? "Stop" : "Start"}
+              </button>
 		</div>      
 	);
      
