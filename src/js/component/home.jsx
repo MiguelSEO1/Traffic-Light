0cba6717bb0322fdfc2ae1ae0b090937324c6d57
Miguel. Á. Ceballos <113799625+MiguelSEO1@users.noreply.github.com>
2023-07-24 13:24:25 +0200

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,60 +1,101 @@
-import React, {useState, useEffect} from "react";
-import Light from "./lights";
+import React, { useState, useEffect } from "react";
+import Lights from "./lights";
+
+//include images into your bundle
+import carretera from "../../img/carretera.jpg";
 
 //create your first component
 const Home = () => {
-	const [lightUp, setLightUp] = useState (0);
-	const [running, setRunning] = useState(false);
-	const lights = ["danger", "warning", "success"];
+  const [lucesSemaforo, setLucesSemaforo] = useState(0);
+  const [coloresSemaforo, setColoresSemaforo] = useState([
+    "danger",
+    "warning",
+    "success",
+  ]);
+  const [addLuz, setAddLuz] = useState(false);
+  const [running, setRunning] = useState(false);
+  const withBackground = {
+    backgroundImage: `URL(${carretera})`,
+  };
 
-	const startLights = (index) => {
-		if (running === false ) {
-		setLightUp(index);
-		setTimeout(() => {
-		const nextIndex = (index + 1) % lights.length;
-		startLights(nextIndex);
-		}, 1000);
-		}
-		}
-		
-		useEffect(() => {
-			let timeoutId = null;
-			
-			const startLights = (index) => {
-			setLightUp(index);
-			if (running) {
-			const nextIndex = (index + 1) % lights.length;
-			timeoutId = setTimeout(() => startLights(nextIndex), 100);
-			}
-			}
-			
-			if (running) {
-			startLights(0);
-			} else {
-			clearTimeout(timeoutId);
-			}
-			
-			return () => clearTimeout(timeoutId);
-			}, [running]);
+  const automatico = () => {
+    setLucesSemaforo((index) => (index + 1) % coloresSemaforo.length);
+  };
 
-			
+  useEffect(() => {
+    let intervalId;
+    if (running) {
+      intervalId = setInterval(automatico, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
 
-	return (
-		<div className="text-center">
-		      <div className="trafficlight">
-                  {lights.map((x, i) =>{
-		              return <Light key={i} index={i} light={lightUp} color={x} click={setLightUp}/>
-	              })}
-              </div>
-		      <button className="btn btn-primary m-3" onClick={() => {
-				lightUp == 2 ? setLightUp(0) : setLightUp(lightUp +1)
-			  }}>Traffic Lights</button>
-			  <button className={`btn ${running ? "btn-danger" : "btn-success"} m-3` } onClick={() => setRunning(!running)}>{running ? "Stop" : "Start"}
-                      </button>
-		</div>      
-	);
-     
-			}
+    return () => clearInterval(intervalId);
+  }, [running]);
+
+  return (
+    <section className="withBackground " style={withBackground}>
+      <div>
+        <div className="paloSemaforo"></div>
+        <div className="cajaSemaforo">
+          {coloresSemaforo.map((e, i) => {
+            return (
+              <Lights
+                index={i}
+                key={i}
+                lucesSemaforo={lucesSemaforo}
+                element={e}
+                setLucesSemaforo={setLucesSemaforo}
+              />
+            );
+          })}
+        </div>
+        <div className="row d-flex align-items-center text-center row ">
+          <div className="col">
+            <button
+              className="btn btn-warning "
+              onClick={() => {
+                setLucesSemaforo((lucesSemaforo + 1) % coloresSemaforo.length);
+              }}
+            >
+              Cambiar
+            </button>
+          </div>
+          <div className="col">
+            <button
+              className={running ? "btn btn-secondary " : "btn btn-danger "}
+              onClick={() => {
+                setRunning(!running);
+              }}
+            >
+              {running ? "STOP" : "Automatic"}
+            </button>
+          </div>
+          <div className="col">
+            <button
+              className={`${addLuz ? `btn btn-success ` : `btn btn-info  `}`}
+              onClick={() => {
+                if (coloresSemaforo.includes("info")) {
+                  // Si el color "info" está presente, lo eliminamos
+                  setColoresSemaforo(
+                    coloresSemaforo.filter((color) => color !== "info")
+                  );
+                  setAddLuz(false);
+                } else {
+                  // Si el color "info" no está presente, lo añadimos
+                  setColoresSemaforo([...coloresSemaforo, "info"]);
+                  setAddLuz(true);
+                }
+              }}
+            >
+              {`${addLuz ? `Eliminar ` : `Añadir `} `}
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
 
