f0d455594e1bbbd85694d63e67a661592cf12696
Miguel. Á. Ceballos <113799625+MiguelSEO1@users.noreply.github.com>
2023-07-28 22:39:18 +0200

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -17,20 +17,33 @@ const Home = () => {
   const [off, setOff] = useState(true);
   const [indiceSecuencia, setIndiceSecuencia] = useState(0);
 
+
   const withBackground = {
     backgroundImage: `URL(${carretera})`,
   };
 
   useEffect(() => {
     let intervalId;
-    if (running) {
-      intervalId = setInterval(() => {
-        setLucesSemaforo(indiceSecuencia);
-        setIndiceSecuencia((index) => (index + 1) % coloresSemaforo.length);
-      }, 1000);
+    if (running && ! off) {
+      if(indiceSecuencia == 0){
+        setLucesSemaforo(indiceSecuencia)
+        intervalId = setInterval(() => {
+          setIndiceSecuencia((index) => (index + 1) % coloresSemaforo.length);
+        }, 1000);
+      }else{
+        intervalId = setInterval(() => {
+          setLucesSemaforo((index) => (index + 1) % coloresSemaforo.length);
+        }, 1000);
+      }
+      
     }
     return () => clearInterval(intervalId);
   }, [running, lucesSemaforo, indiceSecuencia]);
+
+  useEffect(() => {
+    if(!off)
+    setLucesSemaforo(indiceSecuencia);
+  }, [indiceSecuencia])
 
   return (
     <section className="withBackground " style={withBackground}>
@@ -47,45 +60,17 @@ const Home = () => {
                 setLucesSemaforo={setLucesSemaforo}
                 setOff={setOff}
                 setRunnig={setRunning}
+                setIndiceSecuencia={setIndiceSecuencia}
+                off={off}
               />
             );
           })}
         </div>
         <div className=" panel row d-flex align-items-center text-center g-2 py-2 ">
-          <div className=" col-sm-6">
-            <button
-              className="btn btn-warning btn-block"
-              onClick={() => {
-                setOff(false);
-                setRunning(false);
-                setLucesSemaforo(indiceSecuencia);
-                setIndiceSecuencia(
-                  (index) => (index + 1) % coloresSemaforo.length
-                );
-              }}
-            >
-              Cambiar
-            </button>
-          </div>
-          <div className=" col-sm-6">
+        <div className=" col-sm-6">
             <button
               className={
-                running
-                  ? "btn btn-success btn-block"
-                  : "btn btn-danger btn-block"
-              }
-              onClick={() => {
-                setRunning(!running);
-                setOff(false);
-              }}
-            >
-              {running ? "STOP" : "Automatic"}
-            </button>
-          </div>
-          <div className=" col-sm-6">
-            <button
-              className={
-                off ? "btn btn-danger btn-block" : "btn btn-primary btn-block"
+                off ? " on btn btn-danger btn-block px-3" : "btn btn-warning btn-block px-3"
               }
               onClick={() => {
                 setOff(!off);
@@ -105,8 +90,48 @@ const Home = () => {
           </div>
           <div className=" col-sm-6">
             <button
+              className="btn btn-warning btn-block"
+              onClick={() => {
+                setRunning(false);
+                if(!off){
+                  setOff(false);
+                  if(indiceSecuencia == 0){
+                    setLucesSemaforo(indiceSecuencia)
+                  setIndiceSecuencia(
+                    (x) => (x + 1) % coloresSemaforo.length
+                  )
+                  }else {
+                    setOff(false);
+                    setLucesSemaforo(
+                      (x) => (x + 1) % coloresSemaforo.length
+                    )
+                  }
+                }
+                
+                
+              }}
+            >
+              Cambiar
+            </button>
+          </div>
+          <div className=" col-sm-6">
+            <button
+              className={
+                running && !off
+                  ? "btn btn-primary btn-block"
+                  : "btn btn-danger btn-block"
+              }
+              onClick={() => {
+                setRunning(!running);
+              }}
+            >
+              {running && !off ? "STOP" : "Automatic"}
+            </button>
+          </div>
+          <div className=" col-sm-6">
+            <button
               className={`${
-                addLuz ? `btn btn-warning btn-block` : `btn btn-info btn-block`
+                addLuz ? `btn btn-dark btn-block` : `btn btn-info btn-block`
               }`}
               onClick={() => {
                 if (coloresSemaforo.includes("info")) {
@@ -132,4 +157,3 @@ const Home = () => {
 };
 
 export default Home;
-
